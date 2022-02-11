4e5b9beb171815148208557ebae6530990e300c6
Vrentadax <vrentadax@outlook.com>
2022-02-11 13:14:02 -0700

    begun fleshing out functions

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Array of objects for q&a?
+// Start function that starts timer/quiz
+// for loop function to cycle through questions if correct: next q / else next q & timer -- 5s
+// end function check if qa array empty/timer === 0
+// function to save hs - second script for loading hs.html scores?
+
+var timerEl = document.getElementById('timer');
+var quizEl = document.getElementById('quiz');
+
